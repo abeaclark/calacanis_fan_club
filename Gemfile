a0ec3a6aa9a9f1e6f38947bee6bcd7581064074d
abe_clark <abeaclark@gmail.com>
2016-02-28 02:24:43 -0800

    switch db to postgres for Heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'bootstrap-sass'
+gem 'pg'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160228084309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "counts", force: :cascade do |t|
     t.integer  "number"
     t.datetime "created_at", null: false
